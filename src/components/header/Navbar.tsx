7c098da8bca0cc91563e57fd43ce0d669d2bd376
Youssef Abbas <youssefabbasawad@gmail.com>
2024-01-16 16:01:21 +0330

    fourth lesson

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Link from "next/link";
 import styles from './header.module.css';
 import { GrTechnology } from "react-icons/gr";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             <div>
@@ -11,15 +17,25 @@ const Navbar = () => {
                     <GrTechnology />
                     HOSTING
                 </Link>
+                <div className={styles.menu}>
+                    {toggle ? (<IoMdClose onClick={() => setToggle(prev => !prev)} />) : (<AiOutlineMenu onClick={() => setToggle(prev => !prev)} />)}
+                </div>
             </div>
-            <ul className={styles.navLinks}>
-                <Link className={styles.navLink} href="/">Home</Link>
-                <Link className={styles.navLink} href="/articles">Articles</Link>
-                <Link className={styles.navLink} href="/about">About</Link>
-                <Link className={styles.navLink} href="/admin">Admin Dashboard</Link>
-            </ul>
+            <div
+                className={styles.navLinksWrapper}
+                style={{
+                    clipPath: toggle && "polygon(0 0, 100% 0, 100% 100%, 0 100%)" || ""
+                }}
+            >
+                <ul className={styles.navLinks}>
+                    <Link onClick={() => setToggle(false)} className={styles.navLink} href="/">Home</Link>
+                    <Link onClick={() => setToggle(false)} className={styles.navLink} href="/articles">Articles</Link>
+                    <Link onClick={() => setToggle(false)} className={styles.navLink} href="/about">About</Link>
+                    <Link onClick={() => setToggle(false)} className={styles.navLink} href="/admin">Admin Dashboard</Link>
+                </ul>
+            </div>
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
