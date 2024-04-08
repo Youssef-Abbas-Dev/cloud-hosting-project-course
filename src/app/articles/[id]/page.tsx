5751591f9a9f648f45a571f17e7fd2bd5713416a
Youssef Abbas <youssefabbasawad@gmail.com>
2024-04-08 13:07:21 +0330

    sending query to db from server component

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,9 +1,11 @@
-import { getSingleArticle } from "@/apiCalls/articleApiCall";
+//import { getSingleArticle } from "@/apiCalls/articleApiCall";
 import AddCommentForm from "@/components/comments/AddCommentForm";
 import CommentItem from "@/components/comments/CommentItem";
 import { SingleArticle } from "@/utils/types";
 import { cookies } from "next/headers";
 import { verifyTokenForPage } from "@/utils/verifyToken";
+import prisma from "@/utils/db";
+import { redirect } from "next/navigation";
 
 
 interface SingleArticlePageProps {
@@ -14,7 +16,29 @@ const SingleArticlePage = async ({ params }: SingleArticlePageProps) => {
     const token = cookies().get("jwtToken")?.value || "";
     const payload = verifyTokenForPage(token);
 
-    const article: SingleArticle = await getSingleArticle(params.id);
+    //const article: SingleArticle = await getSingleArticle(params.id);
+
+    const article = await prisma.article.findUnique({
+        where: { id: parseInt(params.id) },
+        include: {
+            comments: {
+                include: {
+                    user: {
+                        select: {
+                            username: true,
+                        }
+                    }
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            }
+        }
+    }) as SingleArticle;
+
+    if(!article){
+       redirect("/not-found");
+    }
 
     return (
         <section className="fix-height container m-auto w-full px-5 pt-8 md:w-3/4">
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,8 @@
 export const ARTICLE_PER_PAGE = 6;
-export const DOMAIN = "https://cloud-hosting-project-course.vercel.app";
+
+const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
+const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+
+export const DOMAIN = process.env.NODE_ENV === 'production' 
+    ? PRODUCTION_DOMAIN
+    : DEVELOPMENT_DOMAIN;
